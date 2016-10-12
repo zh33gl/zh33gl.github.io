be9b1330455c44a7a41cc37ba3475bc14cfddf5f
zhgl <guoliang.zhao@bqvision.com>
2016-10-12 17:38:03 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const categories = [{
+	title: 'JavaScript',
+	href: 'views/js',
+	cls: 'js'
+}, {
+	title: 'HTML5',
+	href: 'views/html5',
+	cls: 'html',
+}, {
+	title: 'CSS3',
+	href: 'views/css3',
+	cls: 'css',
+}, {
+	title: 'Nodejs',
+	href: 'views/nodejs',
+	cls: 'nodejs',
+}, {
+	title: 'UI',
+	href: 'views/ui',
+	cls: 'ui',
+}, {
+	title: 'C#',
+	href: 'views/csharp',
+	cls: 'csharp',
+}];
+let vm = new Vue({
+	el: '#app',
+	data: {
+		categories: categories
+	},
+	methods: {
+		openCategory: function(e){
+			let target = e.target,
+				href = target.dataset.href;
+			console.log(e, href)
+		}
+	}
+});
